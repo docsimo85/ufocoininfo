27368ea8205102f19d3eb7ab4d1dcd6749a0f9be
SSYSTEM\sgaido <consulente.gaido@serijakala.com>
2018-01-29 15:49:11 +0100

    aggiunte function price

--- execute.php
+++ execute.php
@@ -1,13 +1,15 @@
 <?php
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
-
-if(!$update)
-{
-  exit;
+if(!$update){
+    exit;
 }
-
 $message = isset($update['message']) ? $update['message'] : "";
+$json = file_get_contents('https://chainz.cryptoid.info/ufo/api.dws?q=getdifficulty');
+$json2 = file_get_contents('https://chainz.cryptoid.info/ufo/api.dws?q=getblockcount');
+$json3 = file_get_contents('https://chainz.cryptoid.info/ufo/api.dws?q=ticker.usd');
+$json4 = file_get_contents('https://chainz.cryptoid.info/ufo/api.dws?q=circulating');
+$json5 = file_get_contents('https://chainz.cryptoid.info/ufo/api.dws?q=ticker.btc');
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
 $firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
@@ -15,11 +17,41 @@ $lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name']
 $username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
 $date = isset($message['date']) ? $message['date'] : "";
 $text = isset($message['text']) ? $message['text'] : "";
-
 $text = trim($text);
 $text = strtolower($text);
-
-header("Content-Type: application/json");
-$parameters = array('chat_id' => $chatId, "text" => $text);
-$parameters["method"] = "sendMessage";
+if($text == '!network'){
+    header("Content-Type: application/json");
+    $parameters = array('chat_id' => $chatId,
+        "text" =>
+            '🌎 UFO Coin Real Time Info: 🌎'.chr(10).chr(10).
+            'Current diff: '.json_decode($json,true).chr(10).
+            'Current block: '.json_decode($json2,true).chr(10).
+            //'Current USD Value: '.json_decode($json3,true).chr(10).
+            'Circulating Coins: '.number_format(json_decode($json4,true)).chr(10).chr(10).
+            'For info on this bot type !help'
+    );
+    $parameters["method"] = "sendMessage";
+//$parameters["reply_markup"] = '{ "keyboard": [["uno", "due"], ["tre", "quattro"], ["cinque"]], "resize_keyboard": true, "one_time_keyboard": false}';
+}
+else if($text == '!price'){
+    header("Content-Type: application/json");
+    $parameters = array('chat_id' => $chatId, "text" =>
+        '💰 UFO Coin Value 💰'.chr(10).chr(10).
+        'Current USD Value: '.number_format(json_decode($json3,true)).chr(10).
+        'Current BTC Value: '.number_format(json_decode($json5,true)).chr(10).chr(10).
+        'For info on this bot type !help'
+    );
+    $parameters["method"] = "sendMessage";
+}
+else if($text == '!help'){
+    header("Content-Type: application/json");
+    $parameters = array('chat_id' => $chatId, "text" =>
+        '🕳 UFO Coin Info Bot 🕳'.chr(10).
+        'developed with ❤ by @docsimo85'.chr(10).
+        'Just type !network and bot will reply with real time info about UFO Coin.'.chr(10).
+        'This bot does not require to be admin and it can be added in group'.chr(10).
+        'If you find it useful donations are welcome :) UFO Address: BwJvr6HVnnsHRK7PArc72yrLXYEe52yAYp');
+    $parameters["method"] = "sendMessage";
+}
+;
 echo json_encode($parameters);
