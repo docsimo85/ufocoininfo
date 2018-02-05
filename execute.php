6cf0f18e235f0f484dc8971f8e079a94ba5e9678
SSYSTEM\sgaido <docsimo85@gmail.com>
2018-02-05 10:00:29 +0100

    rimosso rank da network

--- execute.php
+++ execute.php
@@ -32,8 +32,8 @@ if($text == '!network'){
             'Current diff: '.json_decode($json,true).chr(10).
             'Current block: '.json_decode($json2,true).chr(10).
             //'Current USD Value: '.json_decode($json3,true).chr(10).
-            'Circulating Coins: '.number_format(json_decode($json4,true)).chr(10).chr(10).
-            'Rank on CMC: '.json_decode($json6)[0]->rank.chr(10).
+            'Circulating Coins: '.number_format(json_decode($json4,true)).chr(10).
+//            'Rank on CMC: '.json_decode($json6)[0]->rank.chr(10).
             '✅For info on this bot type !help'
     );
     $parameters["method"] = "sendMessage";
