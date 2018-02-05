39221bdccf48ed524c64695deea07d9a3c615a8b
SSYSTEM\sgaido <docsimo85@gmail.com>
2018-02-05 13:45:33 +0100

    fix punti e virgole x separare migliaia

--- execute.php
+++ execute.php
@@ -51,7 +51,7 @@ else if (strpos($text,'!wallet') === 0) {
 //        'Current USD Value: '.number_format(json_decode($json3,true),5).chr(10).
 //        'Current BTC Value: '.number_format(json_decode($json5,true),5).chr(10).chr(10).
 //        '✅For info on this bot type !help'
-            '💰 Current wallet balance: '. number_format(file_get_contents($urlcheckwallet),6,',','.').chr(10).chr(10).
+            '💰 Current wallet balance: '. number_format(file_get_contents($urlcheckwallet),6,'.',',').chr(10).chr(10).
 //            '📉 Rich rank: '. number_format(file_get_contents($richlist),0,',','.').chr(10).chr(10).
             '✅For info on this bot type !help'
         );
